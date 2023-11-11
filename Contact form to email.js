1da77f37aa6f34fd0a910816327bdf72745cb31e
Abdulrahman Alsaudi <116046596+AbdulrahmanAlsaudi@users.noreply.github.com>
2023-11-12 00:21:47 +0300

    Add files via upload

---
+++ Contact form to email.js
@@ -0,0 +1,28 @@
+function emailSend(){
+
+	var userName = document.getElementById('name').value;
+	var phone = document.getElementById('phone').value;
+	var email = document.getElementById('email').value;
+
+	var messageBody = "Name " + userName +
+	"<br/> Phone " + phone +
+	"<br/> Email " + email;
+	Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "aav26608@gmail.com",
+  Password :"3FC1344855E81365E8A07CAF822BF488C4FA",
+    To : 'abdulrahmanalsaudi2@gmail.com',
+    From : "aav26608@gmail.com",
+    Subject : "This is the subject",
+    Body : messageBody
+}).then(
+  message => {
+  	if(message=='OK'){
+  		swal("Secussful", "You clicked the button!", "success");
+  	}
+  	else{
+  		swal("Error", "You clicked the button!", "error");
+  	}
+  }
+);
+}
